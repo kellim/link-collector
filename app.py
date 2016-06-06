a2ada3208395801b0df80f32af84678fbf432b4a
kellim <kelli.blalock2@gmail.com>
2016-06-05 22:59:02 -0700

    Add links template

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import (Flask, render_template, request, redirect, url_for, flash,
+                   jsonify, abort)
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Render the index page"""
     return render_template('index.html')
+
+@app.route('/links/<collection>/')
+@app.route('/links/<collection>/<category>/')
+def showCategoryLinks(collection, category="default"):
+    """Render the links page for selected collection and category"""
+    if category == "default":
+        category = "set a default category"
+    return render_template('links.html', collection =  collection,
+                           category= category)
 
 if __name__ == '__main__':
     app.debug = True
