2268332b329b7c085021f230c49e563ba6f831d2
kellim <kellim@users.noreply.github.com>
2016-06-30 16:29:04 -0700

    Add modals to confirm deletion
    Added modals for confirming deletion of collections, categories,
    and links.

--- static/scripts.js
+++ static/scripts.js
@@ -2,3 +2,27 @@
 $("#select-coll").change(function() {
   $("#frm-select-coll").submit();
 });
+
+var url = window.location.href;
+
+// Open modal after user clicks delete collection
+if (url.search("_del-coll") >= 0) {
+    $('#collectiondelete').modal('toggle');
+}
+
+// Open modal after user clicks delete link
+if (url.search("_del-link") >= 0) {
+    $('#linkdelete').modal('toggle');
+}
+
+// When the collection delete modal closes, go back to main
+// index url instead of _delete/<collection> url.
+$('#collectiondelete').on('hidden.bs.modal', function () {
+  window.location.replace("../");
+})
+
+// When the link delete modal closes, go back to
+// category/links page instead of _delete/<collection> url.
+$('#linkdelete').on('hidden.bs.modal', function () {
+  window.location.replace("../");
+})
