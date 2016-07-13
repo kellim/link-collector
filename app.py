258e2fbedbe1c2af2fdaa5b9b53eff7ea7272e9b
kellim <kellim@users.noreply.github.com>
2016-07-13 16:37:09 -0700

    Store FB App ID in variable

--- app.py
+++ app.py
@@ -27,9 +27,13 @@ app = Flask(__name__)
 csrf.init_app(app)
 Bootstrap(app)
 
+# Google+ Client ID
 CLIENT_ID = json.loads(
     open('client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Link Collector"
+# Facebook App ID
+FB_APP_ID = json.loads(open('fb_client_secrets.json', 'r').read())[
+        'web']['app_id']
 
 engine = create_engine('sqlite:///links.db')
 Base.metadata.bind = engine
@@ -228,6 +232,7 @@ def contact():
     """Render contact page"""
     collections = session.query(Collection)  # Needed for sidebar
     return render_template('contact.html', collections=collections)
+
 
 @app.route('/help')
 def help():
@@ -786,7 +791,9 @@ def login():
                     (string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
-    return render_template('login.html', CLIENT_ID=CLIENT_ID)
+    return render_template('login.html',
+                           CLIENT_ID=CLIENT_ID,
+                           FB_APP_ID=FB_APP_ID)
 
 
 @csrf.exempt
@@ -907,12 +914,10 @@ def fbconnect():
     access_token = request.data
     print "access token received %s " % access_token
 
-    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
-        'web']['app_id']
     app_secret = json.loads(
         open('fb_client_secrets.json', 'r').read())['web']['app_secret']
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
-        app_id, app_secret, access_token)
+        FB_APP_ID, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
 
